ada9e867c1f1f8eb1c1c10b2e7db9feca8f41097
Karla Porras <32964275+kporras143@users.noreply.github.com>
2018-08-02 10:51:44 -0600

    Update modalLogic.js

--- BeehiveClient/modalLogic.js
+++ BeehiveClient/modalLogic.js
@@ -1,22 +1,18 @@
 window.addEventListener('load', init, false);
 
 function init() {
-    // Get the modal
     var beeModal = document.getElementById('beeModal');
     var postModal = document.getElementById('postModal');
     var toDoModal = document.getElementById('toDoModal');
 
-    // Get the button that opens the modal
     var beeBtn = document.getElementById("beeBtn");
     var postBtn = document.getElementById("postBtn");
     var toDoBtn = document.getElementById("toDoBtn");
 
-    // Get the <span> element that closes the modal
     var spanBee = document.getElementsByClassName("closeBee")[0];
     var spanPost = document.getElementsByClassName("closePost")[0];
     var spanToDo = document.getElementsByClassName("closeToDo")[0];
 
-    // When the user clicks on the button, open the modal 
     beeBtn.onclick = function () {
         beeModal.style.display = "block";
     }
@@ -29,7 +25,6 @@ function init() {
         toDoModal.style.display = "block";
     }
 
-    // When the user clicks on <span> (x), close the modal
     spanBee.onclick = function () {
         beeModal.style.display = "none";
     }
@@ -42,9 +37,4 @@ function init() {
         toDoModal.style.display = "none";
     }
 
-    /*window.onclick = function (event) {
-        if (event.target == beeModal || event.target == postModal || event.target == toDoModal) {
-            modal.style.display = "none";
-        }
-    }*/
 }
